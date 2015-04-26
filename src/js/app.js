82010a5f51cf994261a866caaa25ca0ce6258550
mary <grimesmea@gmail.com>
2015-04-26 18:02:11 -0400

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,82 @@
+$(function() {
+  /**
+   * Includes functionality to setup the Google Map, use geolocation, and stores
+   * map options.
+   */
+  var MapManager = {
+  	latLng: null,
+  	mapOptions: {},
+  	myMap: null,
+
+  	init: function() {
+  		if(typeof google === 'object' && typeof google.maps === 'object') {
+        this.createMap();
+  			this.findUserLocation();
+  		} else {
+  			$('#map-canvas').append('Oops...something went wrong. Try back later!');
+  		}
+  	},
+
+  	createMap: function() {
+  		latLng = new google.maps.LatLng(53,	6);
+  		mapOptions = {
+  									  center: latLng,
+  										zoom: 15,
+  										mapTypeId: google.maps.MapTypeId.ROADMAP,
+  										zoomControl: true,
+  										zoomControlOptions: {
+  										style: google.maps.ZoomControlStyle.DEFAULT
+  										}
+  									};
+
+  		myMap = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  	},
+
+  	findUserLocation: function() {
+  		/**
+  		* Try HTML5 geolocation, if request to share location is not granted by the
+  		* user, this code will not run and default latLng will be used.
+  		*/
+  		if(navigator.geolocation) {
+  			navigator.geolocation.getCurrentPosition(function(position) {
+  				var pos = new google.maps.LatLng(position.coords.latitude,
+  																				position.coords.longitude);
+  				var infowindow = new google.maps.InfoWindow({
+  					map: myMap,
+  					position: pos,
+  					content: 'Location found!'
+  				});
+  				myMap.setCenter(pos);
+  			}, function() {
+  				handleNoGeolocation(true);
+  			});
+  		} else {
+  			// Browser doesn't support Geolocation
+  			handleNoGeolocation(false);
+  		}
+
+  		var handleNoGeolocation = function(errorFlag) {
+  			var content = '';
+  			if(errorFlag) {
+  				content = 'Error: The Geolocation service failed :/';
+  			} else {
+  				content = 'Error: Your browser doesn\'t support geolocation :(';
+  			}
+  			var options = {
+  				map: myMap,
+  				position: latLng,
+  				content: content
+  			};
+  			var infowindow = new google.maps.InfoWindow(options);
+  			map.setCenter(options.position);
+  		};
+  	}
+  };
+
+	var ViewModel = function() {
+		var self = this;
+	};
+
+  MapManager.init();
+  ko.applyBindings(new ViewModel());
+});

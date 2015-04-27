58970454bb25dc991a6c0608653424ff44652434
mary <grimesmea@gmail.com>
2015-04-27 14:05:51 -0400

    Fix formatting

--- src/js/app.js
+++ src/js/app.js
@@ -20,14 +20,14 @@ $(function() {
   	createMap: function() {
   		latLng = new google.maps.LatLng(53.2734, -7.778320310000026);
   		mapOptions = {
-  									  center: latLng,
-  										zoom: 8,
-  										mapTypeId: google.maps.MapTypeId.ROADMAP,
-  										zoomControl: true,
-  										zoomControlOptions: {
-  										style: google.maps.ZoomControlStyle.DEFAULT
-  										}
-  									};
+  			center: latLng,
+  			zoom: 8,
+  			mapTypeId: google.maps.MapTypeId.ROADMAP,
+  			zoomControl: true,
+  			zoomControlOptions: {
+  		    style: google.maps.ZoomControlStyle.DEFAULT
+  		  }
+  		};
 
   		this.map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
   	},

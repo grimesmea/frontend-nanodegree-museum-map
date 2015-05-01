7de9fc7fda91126e4a37f09cbba9a8378d19ce09
mary <grimesmea@gmail.com>
2015-05-01 09:30:19 -0400

    Add functionality to create places with observable properties which are added to the ViewModel as part of an observableArray
    Reformatted code as a result of moving jQuery call to kick-off the app

--- src/js/app.js
+++ src/js/app.js
@@ -1,139 +1,144 @@
-$(function() {
-  /**
-   * Includes functionality to setup the Google Map, use geolocation, and stores
-   * map options.
-   */
-  var MyMap = {
-  	latLng: null,
-  	mapOptions: {},
-  	myMap: null,
+/**
+ * Includes functionality to setup the Google Map, use geolocation, and stores
+ * map options.
+ */
+var MyMap = {
+	latLng: null,
+	mapOptions: {},
+	myMap: null,
+	observablePlacesArray: null,
 
-  	init: function() {
-      if(typeof google === 'object' && typeof google.maps === 'object') {
-        this.createMap();
-  			//this.findUserLocation();
-  		} else {
-  			$('#map-canvas').append('Oops...something went wrong. Try back later!');
-  		}
+  init: function(observablePlacesArray) {
+		this.observablePlacesArray = observablePlacesArray;
 
+    if(typeof google === 'object' && typeof google.maps === 'object') {
+      this.createMap();
+			//this.findUserLocation();
+		} else {
+			$('#map-canvas').append('Oops...something went wrong. Try back later!');
+		}
+	},
 
-  	},
+  createMap: function() {
+		this.latLng = new google.maps.LatLng(53.348, -6.2597);
+		this.mapOptions = {
+			center: this.latLng,
+			zoom: 15,
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			zoomControl: true,
+			zoomControlOptions: {
+		    style: google.maps.ZoomControlStyle.DEFAULT
+		  }
+		};
 
-  	createMap: function() {
-  		this.latLng = new google.maps.LatLng(53.348, -6.2597);
-  		this.mapOptions = {
-  			center: this.latLng,
-  			zoom: 16,
-  			mapTypeId: google.maps.MapTypeId.ROADMAP,
-  			zoomControl: true,
-  			zoomControlOptions: {
-  		    style: google.maps.ZoomControlStyle.DEFAULT
-  		  }
-  		};
+    this.map = new google.maps.Map(document.getElementById("map-canvas"), this.mapOptions);
+    google.maps.event.addListener(MyMap.map, 'idle', function() {
+        Places.getPlaces();
+    });
+	},
 
-  		this.map = new google.maps.Map(document.getElementById("map-canvas"), this.mapOptions);
+  findUserLocation: function() {
+		/**
+		* Try HTML5 geolocation, if request to share location is not granted by the
+		* user, this code will not run and default latLng will be used.
+		*/
+		if(navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(function(position) {
+				var pos = new google.maps.LatLng(position.coords.latitude,
+																				position.coords.longitude);
+				var infowindow = new google.maps.InfoWindow({
+					map: map,
+					position: pos,
+					content: 'Location found!'
+				});
 
-      google.maps.event.addListener(MyMap.map, 'idle', function() {
-          Places.init();
-      });
-  	},
+        map.setCenter(pos);
+			}, function() {
+				handleNoGeolocation(true);
+			});
+		} else {
+			// Browser doesn't support Geolocation
+			handleNoGeolocation(false);
+		}
 
-  	findUserLocation: function() {
-  		/**
-  		* Try HTML5 geolocation, if request to share location is not granted by the
-  		* user, this code will not run and default latLng will be used.
-  		*/
-  		if(navigator.geolocation) {
-  			navigator.geolocation.getCurrentPosition(function(position) {
-  				var pos = new google.maps.LatLng(position.coords.latitude,
-  																				position.coords.longitude);
-  				var infowindow = new google.maps.InfoWindow({
-  					map: map,
-  					position: pos,
-  					content: 'Location found!'
-  				});
-  				map.setCenter(pos);
-  			}, function() {
-  				handleNoGeolocation(true);
-  			});
-  		} else {
-  			// Browser doesn't support Geolocation
-  			handleNoGeolocation(false);
-  		}
+		var handleNoGeolocation = function(errorFlag) {
+			var content = '';
 
-  		var handleNoGeolocation = function(errorFlag) {
-  			var content = '';
-  			if(errorFlag) {
-  				content = 'Error: The Geolocation service failed :/';
-  			} else {
-  				content = 'Error: Your browser doesn\'t support geolocation :(';
-  			}
-  			var options = {
-  				map: map,
-  				position: latLng,
-  				content: content
-  			};
-  			var infowindow = new google.maps.InfoWindow(options);
-  			map.setCenter(options.position);
-  		};
-  	}
-  };
+      if(errorFlag) {
+				content = 'Error: The Geolocation service failed :/';
+			} else {
+				content = 'Error: Your browser doesn\'t support geolocation :(';
+			}
 
-  /**
-   * Gets and stores places using Google Places API based on the map's current
-   * bounds.
-   */
-  var Places = {
-    myPlaces: [],
-    placeIds: new Set(),
+      var options = {
+				map: map,
+				position: latLng,
+				content: content
+			};
 
-    init: function() {
-      this.getPlaces();
-    },
+      var infowindow = new google.maps.InfoWindow(options);
+			map.setCenter(options.position);
+		};
+	}
+};
 
-    getPlaces: function() {
-      var services;
-      var mapBounds = MyMap.map.getBounds();
+/**
+ * Gets and stores places using Google Places API based on the map's current
+ * bounds.
+ */
+var Places = {
+  myPlaces: [],
+  placeIds: new Set(),
 
-      var request = {
-        location: MyMap.latLng,
-        bounds: mapBounds,
-        rankBy: google.maps.places.RankBy.PROMINENCE,
-        types: ['museum']
-      };
+  getPlaces: function() {
+    var services;
+    var mapBounds = MyMap.map.getBounds();
 
-      service = new google.maps.places.PlacesService(MyMap.map);
-      service.nearbySearch(request, this.callback);
-    },
+    var request = {
+      location: MyMap.latLng,
+      bounds: mapBounds,
+      rankBy: google.maps.places.RankBy.PROMINENCE,
+      types: ['museum']
+    };
 
-    callback: function(results, status) {
-      if(status == google.maps.places.PlacesServiceStatus.OK) {
-        for(var i = 0; i < results.length; i++) {
-          var myPlace = results[i];
+    service = new google.maps.places.PlacesService(MyMap.map);
+    service.nearbySearch(request, this.callback);
+  },
 
-          if(!Places.placeIds.has(myPlace.place_id)) {
-            Places.placeIds.add(myPlace.place_id);
-            Places.myPlaces.push(myPlace);
-            Places.createMapMarker(myPlace);
-            console.log(myPlace.name);
-          }
+  callback: function(results, status) {
+    if(status == google.maps.places.PlacesServiceStatus.OK) {
+      for(var i = 0; i < results.length; i++) {
+        var myPlace = results[i];
+
+        if(!Places.placeIds.has(myPlace.place_id)) {
+          Places.placeIds.add(myPlace.place_id);
+          Places.myPlaces.push(myPlace);
+          console.log(myPlace.name);
+
+					MyMap.observablePlacesArray.push(new Place(myPlace));
         }
       }
-    },
-
-    createMapMarker: function(place) {
-      var marker = new google.maps.Marker({
-        position: place.geometry.location,
-        map: MyMap.map,
-        title: place.name
-      });
     }
-  };
+  }
+};
 
-	var ViewModel = function() {
-		var self = this;
-	};
+var Place = function(data) {
+	var self = this;
 
-  MyMap.init();
+	this.name = ko.observable(data.name);
+  this.latLng = ko.observable(data.geometry.location);
+};
+
+
+var ViewModel = function() {
+	var self = this;
+
+  this.places = ko.observableArray([]);
+  this.currentPlace = ko.observable();
+
+  MyMap.init(self.places);
+};
+
+$(function() {
   ko.applyBindings(new ViewModel());
 });
